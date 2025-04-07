c3fff45432445ce1e1ef70fc2b05f54561070e67
Mikolaj Wachnicki <mikolaj6wachnicki@gmail.com>
2025-04-07 18:22:48 +0000

    Gotowe

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -12,25 +12,21 @@ def generate_launch_description():
     yaml_file = os.path.join(get_package_share_directory('my_robot_rviz'), 'config', 'robot_params.yaml')
     with open(yaml_file, 'r') as file:
         params = yaml.safe_load(file)
+
     urdf_file = os.path.join(get_package_share_directory('my_robot_rviz'), 'urdf', 'my_robot.urdf.xacro')
     with open(urdf_file, 'r') as infp:
         robot_desc = infp.read()
 
-    robot_description = Command(['xacro', urdf_file])
+    rviz_config_path = os.path.join(
+            get_package_share_directory('my_robot_rviz'),
+            'config',
+            'my_robot.rviz'
+        )
+
+    robot_description = Command(['xacro',' ', urdf_file])
 
     return LaunchDescription([
 
-    ExecuteProcess(
-                cmd=['ros2', 'run', 'my_robot_rviz', 'forward_kinematics'],
-                output='screen'
-            ),
-    
-            # Uruchomienie komendy `ros2 topic echo` (można to zrobić w tle)
-     ExecuteProcess(
-                cmd=['ros2', 'topic', 'echo', '/dobot/end_effector_pose'],
-                output='screen',
-                shell=True
-    ),
     Node(
                 package='my_robot_rviz',
                 executable='forward_kinematics',
@@ -43,37 +39,29 @@ def generate_launch_description():
             name='joint_state_publisher_gui',
             output='screen',
             parameters=[{'robot_description': ParameterValue(
-                        Command(['xacro ', str(urdf_file)]), value_type=str
-                    )
-            }]
+                        robot_description, value_type=str
+                    )}]
     ),
  	Node(
             package='tf2_ros',
             executable='static_transform_publisher',
-            arguments=['0', '0', '0.025', '0', '0', '0', 'map', 'base_link'],
+            arguments=['0', '0', '0.025', '3.14159', '0', '0', 'map', 'base_link'],
             output='screen'
-	),
-	Node(
-    	    package="tf2_ros",
-    	    executable="static_transform_publisher",
-    	    arguments=["0", "0", "0", "0", "0", "0", "base_link", "link1"],
-    	    output="screen"
 	),
     Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
 	        name='robot_state_publisher',
             parameters=[{'robot_description': ParameterValue(
-                        Command(['xacro ', str(urdf_file)]), value_type=str
-                    )
-                }],
+                        robot_description, value_type=str
+                    )}],
             output='screen'
     ),
     Node(
             package='rviz2',
             executable='rviz2',
-            name='rviz2',
             output='screen',
+            arguments=['-d', rviz_config_path]
     ),
     ])
 
